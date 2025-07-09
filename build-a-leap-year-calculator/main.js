f34ed1cd95c2fd8e824a64a2df246fecd9c1915e
noooreality <schutz.niklas@icloud.com>
2025-07-09 19:16:34 +0200

    Create main.js

---
+++ build-a-leap-year-calculator/main.js
@@ -0,0 +1,12 @@
+function isLeapYear(year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return `${year} is a leap year.`;
+  } else {
+    return `${year} is not a leap year.`;
+  }
+}
+
+const year = 2028;
+const result = isLeapYear(year);
+
+console.log(result);
